06c20da622e40deb99c179189145efc40a187a02
Mabel Zhou <zym0306@hotmail.com>
2024-06-03 00:56:06 -0400

    First commit

---
+++ server/db/knexfile.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+module.exports = {
+    development: {
+      client: 'mysql2',
+      connection: {
+        host: process.env.DB_LOCAL_HOST,
+        database: process.env.DB_LOCAL_DBNAME,
+        user: process.env.DB_LOCAL_USER,
+        password: process.env.DB_LOCAL_PASSWORD,
+        charset: "utf8",
+      },
+      migrations: {
+        directory: './db/migrations',
+      },
+    },
+  };
+  
